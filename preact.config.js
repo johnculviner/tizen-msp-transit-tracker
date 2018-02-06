54824bfcea29ea5fbf262a855c60715d9016ee28
John Culviner <john.culviner@gmail.com>
2018-02-06 11:51:08 -0600

    Delete preact.config.js

--- preact.config.js
+++
@@ -1,6 +0,0 @@
-// preact.config.js
-export default function (config, env, helpers) {
-  config.output.filename = '[name].js'
-  let { plugin } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0]
-  plugin.options.sourceMap = false
-}
